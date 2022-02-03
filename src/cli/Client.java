133c37c971a6f85635c3a93f94ac1d5a00fbe81e
Luca Broggiato <luca.broggiato@edu.unito.it>
2022-02-03 11:58:07 +0100

    fixed bugs which did not allow the client to connect to the server

--- src/cli/Client.java
+++ src/cli/Client.java
@@ -14,7 +14,7 @@ public class Client {
     private Socket socket;
     private ObjectInputStream in;
     private ObjectOutputStream out;
-    private int idClient;
+    private final int idClient;
     private final int MAX_ATTEMPTS;
     private final int port;
     private final String address;
@@ -28,15 +28,19 @@ public class Client {
 
     public void start() {
         try {
+            System.out.println("Client " + this.idClient + " connecting to server...");
             this.socket = new Socket(this.address, this.port);
+            System.out.println("Client " + this.idClient + " connected to server!");
             int index = 0;
+            boolean go = true;
 
-            while (index < this.MAX_ATTEMPTS) {
+            while (index < this.MAX_ATTEMPTS && go) {
                 this.openStreams();
                 List<Studente> students = this.createStudents(4);
                 this.sendStudents(students);
 
                 List<Studente> editedStudents = this.receiveStudents();
+                go = false;
                 if (editedStudents == null) {
                     System.out.println("The list received via stream is empty");
                 } else {
@@ -45,39 +49,59 @@ public class Client {
                     }
                 }
 
-                index--;
+                index++;
             }
 
         } catch (IOException e) {
             System.out.println("Exception in class Client. Unable to set the socket right: " + e.getMessage());
+        } finally {
+            this.closeStreams();
         }
     }
 
     private void openStreams() {
+        System.out.println("Client " + this.idClient + " connecting streams...");
         try {
-            this.in = new ObjectInputStream(this.socket.getInputStream());
             this.out = new ObjectOutputStream(this.socket.getOutputStream());
-
             this.out.flush();
+
+            this.in = new ObjectInputStream(this.socket.getInputStream());
+            System.out.println("Client " + this.idClient + " streams correctly opened!");
         } catch (IOException e) {
             System.out.println("Exception in class Client. Unable to open streams: " + e.getMessage());
         }
     }
 
+    private void closeStreams() {
+        if (this.socket != null) {
+            try {
+                this.in.close();
+                this.out.close();
+                this.socket.close();
+            } catch (IOException e) {
+                System.out.println("Exception in class Client. Unable to close streams: " + e.getMessage());
+            }
+        }
+    }
+
     private void sendStudents(List<Studente> students) {
+        System.out.println("Client " + this.idClient + " writing on stream...");
         try {
             this.out.writeObject(students);
             this.out.flush();
+            System.out.println("Client " + this.idClient + " streams correctly written!");
         } catch (IOException e) {
             System.out.println("Exception in class Client. Unable to write object into stream: " + e.getMessage());
         }
     }
 
     private List<Studente> receiveStudents() {
+        System.out.println("Client " + this.idClient + " reading from stream...");
         List<Studente> editedStudents = null;
 
         try {
             editedStudents = (List<Studente>) this.in.readObject();
+            System.out.println("Client " + this.idClient + " streams correctly read!");
         } catch (IOException | ClassNotFoundException e) {
             System.out.println("Exception in class Client. Unable to read object from stream: " + e.getMessage());
         }
@@ -93,7 +117,7 @@ public class Client {
         String[] surnames = {"Cocaina", "Broggiato", "Gaviria", "Digrankio", "Ioana", "Frittata", "Stuarda", "Puta"};
 
         for (int index = 0; index < numStudents; index++) {
-            students.add(new Studente(names[r.nextInt()], surnames[r.nextInt()], "Original description"));
+            students.add(new Studente(names[r.nextInt(names.length)], surnames[r.nextInt(surnames.length)], "Original description"));
         }
 
         return students;
